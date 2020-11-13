80a8a6ede552dc3d1be70a59f344f0cf1f8c8096
Victor Hok <victorhok@outlook.com>
2020-11-13 16:59:54 -0500

    Updated for more consistent naming convention. :peanuts:

--- gridxdrop.js
+++ gridxdrop.js
@@ -1,8 +1,9 @@
-// Global variable
-// IMPORTANT: The of cellUnit must be in sync with _variables.scss $cell-unit for uniformity between CSS and JS.
+// ******* GLOBAL VARIABLES *******
+
+// IMPORTANT: The of cellUnit must be in sync with _variables.scss $form__input-cell-unit for uniformity between CSS and JS.
 const cellUnit = 50;
 
-// An array 'item' containing all furniture objects. Global array.
+// An array 'item' containing all furniture objects.
 const item = [{
         sku: 'BEDDBLNEL',
         name: "Neljyak Double Bed",
@@ -55,19 +56,77 @@ const item = [{
         deployed: false
 }];
 
+// Error codes mainly for accessibility users that can't experience full visualization and may require other forms of feedback. These are printed to output field in widget.
 const errorCodes = [
     '',
     ''
 ];
 
-// All the functions you need to manipulate things.
+// ******* NAMESPACE & METHODS *******
+
+// widget: namespace for all user interaction based methods (majority of methods kept under this namespace).
 const widget = {};
 
-// Retrieves the selected item on tool widget's corresponding itemArray element.
+// ******* METHODS *******
+// .getActiveItem(): Retrieves the object from itemArray that corresponds to currently selected item on widget.
 widget.getActiveItem = () => {
     const itemIdArray = item.map((element) => element.id);
     const itemIndex = itemIdArray.indexOf(`${$('input[name="furniture"]:checked').attr('id')}`);
     return item[itemIndex];
+};
+
+// .getRotateReduced(): Converts total rotation value into one of four values representing the item's direction.
+widget.getRotateReduced = (rotateValue) => rotateValue < 0 ? rotateValue % 4 + 4 : rotateValue % 4; 
+
+// .getRotateDirection(): Converts number value of rotation into letter designation like a compass (N, E, S, W).
+// rotation value meanings:
+// 0 -> 'South'
+// 1 -> 'West'
+// 2 -> 'North'
+// 3 -> 'East'
+widget.getRotateDirection = (rotateValueReduced) => {
+    switch (rotateValueReduced) {
+        case 0:
+            return 'South';
+        case 1:
+            return 'West';
+        case 2:
+            return 'North';
+        case 3:
+            return 'East';
+        default:
+    }
+}
+
+// .updateWidgetFields(): 
+widget.updateWidgetFields = () => {
+    $('#tool__form-widget input').on('click', () => {
+        const activeItem = widget.getActiveItem();
+        $('#form__input-cell-x').val(activeItem.position.x);
+        $('#form__input-cell-y').val(activeItem.position.y);
+        $('#form__input-direction').val(widget.getRotateDirection(activeItem.position.rotation));
+    });
+};
+
+widget.updateCellData = () => {
+
+};
+
+widget.updateGridData = () => {
+
+};
+
+
+// ******* EVENT LISTENER METHODS *******
+
+// .updateWidgetFields(): 
+widget.updatetFieldsListener = () => {
+    $('#tool__form-widget .furniture').on('click', () => {
+        const activeItem = widget.getActiveItem();
+        $('#form__input-cell-x').val(activeItem.position.x);
+        $('#form__input-cell-y').val(activeItem.position.y);
+        $('#form__input-direction').val(widget.getRotateDirection(activeItem.position.rotation));
+    });
 };
 
 widget.translateListener = () => {
@@ -75,41 +134,17 @@ widget.translateListener = () => {
 };
 
 widget.rotateListener = () => {
-    // rotation value meanings:
-    // 0 -> 'South'
-    // 1 -> 'West'
-    // 2 -> 'North'
-    // 3 -> 'East'
-
-    // Convert total rotation into one of four values representing direction.
-    getRotateReduced = (rotateValue) => rotateValue < 0 ? rotateValue%4 + 4 : rotateValue%4; 
-    
-    // Convert number value of rotation into letter designation like a compass (N, E, S, W).
-    getRotateDirection = (rotateValueReduced) => {
-        switch(rotateValueReduced) {
-            case 0:
-                return 'South';
-            case 1:
-                return 'West';
-            case 2:
-                return 'North';
-            case 3:
-                return 'East';
-            default:
-        }
-    }
-
     $('#rotate-ccw').on('click', (event) => {
         event.preventDefault();
         const activeItem = widget.getActiveItem();
 
         if(activeItem) {
             activeItem.position.rotation -= 1;
-            const rotationValue = getRotateReduced(activeItem.position.rotation);
-            const rotationText = getRotateDirection(rotationValue);
+            const rotationValue = widget.getRotateReduced(activeItem.position.rotation);
+            const rotationText = widget.getRotateDirection(rotationValue);
 
             activeItem.position.rotation = rotationValue;
-            $('#direction').val(rotationText);
+            $('#form__input-direction').val(rotationText);
         }
     });
 
@@ -119,29 +154,20 @@ widget.rotateListener = () => {
 
         if(activeItem) {
             activeItem.position.rotation += 1;
-            const rotationValue = getRotateReduced(activeItem.position.rotation);
-            const rotationText = getRotateDirection(rotationValue);
+            const rotationValue = widget.getRotateReduced(activeItem.position.rotation);
+            const rotationText = widget.getRotateDirection(rotationValue);
 
             activeItem.position.rotation = rotationValue;
-            $('#direction').val(rotationText);
+            $('#form__input-direction').val(rotationText);
         }
-    });
-};
-
-widget.updateFields = () => {
-    $('#tool__div-widget input').on('click', () => {
-        const activeItem = widget.getActiveItem();
-        $('#cell-x').val(activeItem.position.x);
-        $('#cell-y').val(activeItem.position.y);
-        $('#direction').val(getRotateDirection(activeItem.position.rotation));
     });
 };
 
 widget.moveListener = () => {
     $('form').on('submit', function(event) {
         event.preventDefault();
-        const xCoord = ( $('#cell-x').val() * cellUnit ) + 'px';
-        const yCoord = ( $('#cell-y').val() * cellUnit ) + 'px';
+        const xCoord = ( $('#form__input-cell-x').val() * cellUnit ) + 'px';
+        const yCoord = ( $('#form__input-cell-y').val() * cellUnit ) + 'px';
 
         const activeItem = widget.getActiveItem();
 
@@ -165,23 +191,25 @@ widget.moveListener = () => {
             console.log("can't do that bub");
         }
         
-
-
     });
 };
 
-
+// ******* MAIN APP NAMESPACE *******
 const grid = {};
 // grid properties
 // sizeX: The horizontal length of the grid in units of "cells".
 // sizeY: The vertical length of the grid in units of "cells".
-// cellArray: An array that represents all the cells on the grid.
 grid.sizeX = 0;
 grid.sizeY = 0;
+
+// NOTE: VERY IMPORTANT ARRAY.
+// cellArray: An array that represents all the cells on the grid. 
+// element of cellArray -> [{ occupied: <boolean>, {jQuery object representing cell div} }]
 grid.cellArray = [];
 
-// ***** gridInit *****
-// Description: Sets the grid size based on user input.
+// // ******* INIT METHOD *******
+
+// .init(): Sets the grid size based on user input.
 grid.init = function (x, y) {
     // Purpose: To create a two-dimensional array where cellArray[x][y] accesses the specific cell located at (x, y).
     //
@@ -221,7 +249,7 @@ grid.init = function (x, y) {
     }
 
     // Initialize widget functionality
-    widget.updateFields();
+    widget.updatetFieldsListener();
     widget.rotateListener();
     widget.moveListener();
 };
@@ -232,7 +260,6 @@ grid.init = function (x, y) {
 
 // DOCUMENT READY
 $(function() {
-
     grid.init(10, 10);
-    // grid.cellArray[0][0][<property>];
+
 });
